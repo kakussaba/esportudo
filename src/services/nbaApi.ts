3fb395d415231b7985c80a846e14ba111a47cde6
kakussaba <kananda.kussaba@pinkapp.com>
2022-07-25 18:48:44 -0300

    chore: removes unnecessary async await

--- src/services/nbaApi.ts
+++ src/services/nbaApi.ts
@@ -2,17 +2,17 @@ import { API } from './index';
 import { AxiosResponse } from 'axios';
 import { Players, Teams } from './types';
 
-export const getTeams = async (): Promise<AxiosResponse<Teams>> => {
+export const getTeams = (): Promise<AxiosResponse<Teams>> => {
     try {
-        return await API.get('/teams');
+        return API.get('/teams');
     } catch (error: any) {
         return error;
     }
 }
 
-export const getPlayers = async (id: number): Promise<AxiosResponse<Players>> => {
+export const getPlayers = (id: number): Promise<AxiosResponse<Players>> => {
     try {
-        return await API.get(`/players?team=${id}&season=2021`);
+        return API.get(`/players?team=${id}&season=2021`);
     } catch (error: any) {
         return error;
     }
