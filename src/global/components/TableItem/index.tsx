0b4f5ac938a3eb60ceb3c54c81f1703540a7bb9d
kakussaba <kananda.kussaba@pinkapp.com>
2022-07-25 12:03:21 -0300

    fix: implements unit type enum

--- src/global/components/TableItem/index.tsx
+++ src/global/components/TableItem/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import * as S from './style';
 
+export enum TypeUnit {
+    'meters' = "m",
+    'kilograms' = "kg"
+}
 type TableItemProps = {
     title: string;
     text: string;
+    type?: TypeUnit;
 };
 
 export const TableItem: React.FC<TableItemProps> = ({
     title,
-    text
+    text,
+    type
 }) => {
     return (
         <S.Container>
             <S.Title>{title}</S.Title>
-            <S.Text>{text}</S.Text>
+            <S.Text>{text}{type}</S.Text>
         </S.Container>
     )
 }
--- src/global/theme/default.ts
+++ src/global/theme/default.ts
@@ -4,7 +4,6 @@ export default {
 
         //B
         BLACK: '#000000',
-
         //W
         WHITE: '#FFFFFF',
 
