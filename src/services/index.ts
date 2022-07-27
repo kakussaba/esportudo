dff21869f805ae4f91a06a3cf80535bfdab903c5
kakussaba <kananda.kussaba@pinkapp.com>
2022-07-27 20:47:27 -0300

    chore: set variables

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const API = axios.create({
   baseURL: 'https://api-nba-v1.p.rapidapi.com',
   headers: {
-    "x-rapidapi-key": "8f7a8949ecmshe11411d4c184b1ap1bab26jsnc3939e65391b",
+    "x-rapidapi-key": "__API_VALUE__",
     "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
   }
 });
